4278eb12ce0903da15f21942e2ab993fa7377d3e
Railen-Laminers <railen.laminero@gmail.com>
2026-05-29 21:35:31 +0800

    Configured GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/portfolio2.0/', // IMPORTANT for GitHub Pages
+
   server: {
-    host: '0.0.0.0', // Listen on all network interfaces (LAN)
-    port: 5173,       // Optional: you can change the port if needed
-    // strictPort: true // Uncomment if you want Vite to exit if port is already in use
-  }
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
